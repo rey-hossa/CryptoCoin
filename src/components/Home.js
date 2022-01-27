6d036b1b851c9a1466c71861079bb9b5bd920cec
rey-hossa <78262774+rey-hossa@users.noreply.github.com>
2022-01-27 01:20:42 +0100

    Responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,11 @@ import {Context} from "../App";
 import React, { useContext } from 'react';
 
 import image from '../images/final-image.jpg';
+import image1180 from '../images/final-image-1180.jpg';
+import image950 from '../images/final-image-950.jpg';
+import image700 from '../images/final-image-700.jpg';
+import image550 from '../images/final-image-550.jpg';
+import image450 from '../images/final-image-450.jpg';
 
 import Header from "./Header";
 import PreferedCrypto from "./PreferedCrypto";
@@ -16,7 +21,19 @@ function Home() {
 
   return (
     <div className="home">
-      <img id="wallpaper" src={image} alt="" />
+      <picture >
+        <source media='(max-width: 450px)'
+                srcset={image450}/>
+        <source media='(max-width: 550px)'
+                srcset={image550}/>
+        <source media='(max-width: 700px)'
+                srcset={image700}/>
+        <source media='(max-width: 950px)'
+                srcset={image950}/>
+        <source media='(max-width: 1180px)'
+                srcset={image1180}/>
+        <img id="wallpaper" src={image} />
+      </picture>
 
       <Header/>
       <div className="image_space">
